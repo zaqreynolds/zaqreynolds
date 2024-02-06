4560e2a4e97dd587958e1e32be71190655fb020d
Zaq Reynolds <zaqreynolds@gmail.com>
2024-02-05 16:05:33 -0800

    feat(page): Update page layout and font sizes
    - Updated the layout of the main container to occupy the entire screen width and height.
    - Adjusted the font sizes of headings in the "About", "Work", "Projects", and "Contact" sections for better readability.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 const Home = () => {
   return (
-    <main className="flex p-5 justify-between">
-      <div className="h-full flex flex-col">
+    <main className="h-screen w-screen flex p-5 justify-between">
+      <div className="h-full w-full flex flex-col">
         <h1 className="text-9xl">Zaq Reynolds</h1>
         <p>Full-stack web developer</p>
       </div>
-      <div className="h-full flex flex-col pr-44 mt-32">
-        <h2 className="text-8xl mb-10">About</h2>
-        <h2 className="text-8xl mb-10">Work</h2>
-        <h2 className="text-8xl mb-10">Projects</h2>
-        <h2 className="text-8xl mb-10">Contact</h2>
+      <div className="h-full flex flex-col pr-44 justify-center">
+        <h2 className="text-7xl mb-10">About</h2>
+        <h2 className="text-7xl mb-10">Work</h2>
+        <h2 className="text-7xl mb-10">Projects</h2>
+        <h2 className="text-7xl mb-10">Contact</h2>
       </div>
     </main>
   );
