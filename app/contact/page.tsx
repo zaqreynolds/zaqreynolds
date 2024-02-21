a1c1dabc85b4d950a6e78b42eecc836d2562365d
Zaq Reynolds <zaqreynolds@gmail.com>
2024-02-20 20:29:39 -0800

    Style(contact, projects): Update page layout to use main element with new styling for headings.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,9 +2,9 @@ import Header from "@/components/Header";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <main className="flex flex-col p-5 z-0 overflow-y-auto">
+      <h1 className="text-5xl sm:text-6xl lg:text-8xl xl:text-9xl">Contact</h1>
+    </main>
   );
 };
 export default Contact;
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,9 +2,9 @@ import Header from "@/components/Header";
 
 const Projects = () => {
   return (
-    <div>
-      <h1>Projects</h1>
-    </div>
+    <main className="flex flex-col p-5 z-0 overflow-y-auto">
+      <h1 className="text-5xl sm:text-6xl lg:text-8xl xl:text-9xl">Projects</h1>
+    </main>
   );
 };
 export default Projects;
