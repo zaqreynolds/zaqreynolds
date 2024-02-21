191fea16a49e57a2fae3c1ee206223182e1a675b
Zaq Reynolds <zaqreynolds@gmail.com>
2024-02-20 20:19:59 -0800

    refactor(components): Update Header to use DropDownMenu
    - Replace Header component with DropDownMenu for improved functionality and code organization. Remove redundant code related to menu toggling and outside click handling.

---
+++ components/DropDownMenu.tsx
@@ -0,0 +1,66 @@
+import { HamburgerMenuIcon, Cross1Icon } from "@radix-ui/react-icons";
+import { useEffect, useRef, useState } from "react";
+import Link from "./Link";
+import navList from "@/app/navList";
+import { usePathname } from "next/navigation";
+
+const DropDownMenu = () => {
+  const anchorRef = useRef<HTMLButtonElement>(null);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const [open, setOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const pathname = usePathname();
+
+  const activeNavList = navList.filter(
+    (navItem) => navItem.toLowerCase() !== pathname.slice(1)
+  );
+
+  return (
+    <div>
+      <button className="w-6 h-6 mr-2" onClick={toggleMenu} ref={anchorRef}>
+        {!open && <HamburgerMenuIcon width="20" height="20" />}
+        {open && <Cross1Icon width="20" height="20" />}
+      </button>
+      {open && (
+        <div
+          id="dropdown"
+          ref={dropdownRef}
+          className="z-10 absolute rounded-lg w-40 border border-defaultText pl-2 backdrop-blur-lg "
+        >
+          <ul className="py-2 text-sm ">
+            {activeNavList.map((navItem) => (
+              <Link
+                title={navItem}
+                destination={`/${navItem.toLowerCase()}`}
+                key={navItem}
+                small={true}
+              />
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+export default DropDownMenu;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,41 +1,11 @@
 "use client";
-import { use, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { usePathname } from "next/navigation";
 import Link from "./Link";
-import navList from "@/app/navList";
-import { HamburgerMenuIcon, Cross1Icon } from "@radix-ui/react-icons";
+import DropDownMenu from "./dropDownMenu";
 
 const Header = () => {
-  const [open, setOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setOpen(!open);
-  };
-
-  const dropdownRef = useRef<HTMLDivElement>(null);
-  const anchorRef = useRef<HTMLButtonElement>(null);
-
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      dropdownRef.current &&
-      !dropdownRef.current.contains(event.target as Node)
-    ) {
-      setOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
-
   const pathname = usePathname();
-
-  const activeNavList = navList.filter(
-    (navItem) => navItem.toLowerCase() !== pathname.slice(1)
-  );
 
   const [isHome, setIsHome] = useState(true);
 
@@ -52,31 +22,7 @@ const Header = () => {
       className={`${isHome ? `hidden` : `block`} w-full pt-1 w-full z-50`}
     >
       <div className="px-2 flex justify-center">
-        {/* Burger Menu */}
-        <div>
-          <button className="w-6 h-6 mr-2" onClick={toggleMenu} ref={anchorRef}>
-            {!open && <HamburgerMenuIcon width="20" height="20" />}
-            {open && <Cross1Icon width="20" height="20" />}
-          </button>
-          {open && (
-            <div
-              id="dropdown"
-              ref={dropdownRef}
-              className="z-10 absolute rounded-lg w-40 border border-defaultText pl-2 backdrop-blur-lg "
-            >
-              <ul className="py-2 text-sm ">
-                {activeNavList.map((navItem) => (
-                  <Link
-                    title={navItem}
-                    destination={`/${navItem.toLowerCase()}`}
-                    key={navItem}
-                    small={true}
-                  />
-                ))}
-              </ul>
-            </div>
-          )}
-        </div>
+        <DropDownMenu />
         <div className="grow" />
         <Link title="Zaq Reynolds" destination={"/"} small={true} />
         <div className="grow pr-[32px]" />
