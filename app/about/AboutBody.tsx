2a5cd4e29155b52b970092c067f1c36c85202c2d
Zaq Reynolds <zaqreynolds@gmail.com>
2024-02-20 17:05:32 -0800

    Refactor: Update About component to use separate AboutBody component
    - Extracted content from About component into a new AboutBody component
    - Improved code organization and readability by separating concerns

---
+++ app/about/AboutBody.tsx
@@ -0,0 +1,64 @@
+const AboutBody = () => {
+  return (
+    <>
+      <p>
+        Hello! I&apos;m Zaq Reynolds, a passionate and dedicated Full Stack Web
+        Developer with a history of making pixel-perfect web applications for a
+        variety of uses.
+      </p>
+      <br />
+      <p>
+        With a career spanning over five years in the tech industry, I have
+        developed a robust skill set in building and optimizing web applications
+        that deliver a seamless user experience.
+      </p>
+      <br />
+      <p>
+        From my early days as a Senior Audio Engineer at Sound Emporium in
+        Nashville, where I honed my attention to detail and creative
+        problem-solving skills, to my current role at Utopia Solutions in Los
+        Angeles, my journey has been fueled by a relentless pursuit of
+        excellence and a deep love for technology. My technical expertise
+        includes a wide array of development languages and frameworks such as
+        Node.js, React, Redux, TypeScript, Express.js, Next.js, Tailwind,
+        Bootstrap, and MySQL, among others. I take pride in my ability to
+        translate complex technical challenges into intuitive and visually
+        appealing web solutions.
+      </p>
+      <br />
+      <p>
+        In my role at Utopia Solutions, I&apos;ve had the privilege of working
+        on cutting-edge web and mobile applications, where I collaborated
+        closely with product teams and engineers to develop, test, launch, and
+        support projects that push the boundaries of what&apos;s possible on the
+        web. My tenure at MetaTech.dev as a Front End Developer Intern further
+        solidified my skills in software development, particularly in fintech
+        applications, where I contributed to all aspects of the development
+        process, from design to deployment.
+      </p>
+      <br />
+      <p>
+        Prior to my tech career, I earned a B.A. in Music Production &
+        Engineering from Berklee College of Music and a B.S. in Marketing from
+        Arizona State University. This unique combination of creative and
+        technical education has equipped me with a versatile skill set that I
+        leverage in every project I undertake.
+      </p>
+      <br />
+      <p>
+        Outside of work, I&apos;m an avid golfer who recently broke 90, a Sunday
+        morning tennis player, and a once-aspiring bakery CEO. These interests,
+        while seemingly unrelated to my profession, have taught me the
+        importance of persistence, strategy, and the joy of pursuing one&apos;s
+        passions. Whether I&apos;m coding, collaborating with clients, or
+        hitting a backhand on the tennis court, I bring energy, enthusiasm, and
+        a commitment to excellence to everything I do. I&apos;m always looking
+        for new challenges and opportunities to grow both personally and
+        professionally. Let&apos;s connect and explore how we can create
+        something amazing together.
+      </p>
+    </>
+  );
+};
+
+export default AboutBody;
