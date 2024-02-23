6229cdb002f69aa85881e60b91fa5957658d0864
Zaq Reynolds <zaqreynolds@gmail.com>
2024-02-23 10:20:05 -0800

    style(contact): update social media icons and add LinkedIn icon
    Added a new LinkedIn SVG icon file and updated the Contact page to display both GitHub and LinkedIn icons in a flex layout.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,14 @@ const Contact = () => {
   return (
     <main className="flex flex-col p-5 z-0 overflow-y-auto">
       <h1 className="text-5xl sm:text-6xl lg:text-8xl xl:text-9xl">Contact</h1>
-      <Image src="./github.svg" alt="github" width={35} height={35} />
+      <div className="flex justify-between">
+        <div className="flex bg-defaultText h-[37px] w-[37px] justify-center rounded">
+          <Image src="./github.svg" alt="github" width={34} height={34} />
+        </div>
+        <div className="flex bg-defaultText h-[37px] w-[37px] justify-center rounded">
+          <Image src="./linkedin.svg" alt="linkedin" width={35} height={35} />
+        </div>
+      </div>
     </main>
   );
 };
