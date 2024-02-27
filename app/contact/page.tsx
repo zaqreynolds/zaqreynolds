5b6bf595b7ab16a7b2fc6c168effbc1d7de11122
Zaq Reynolds <zaqreynolds@gmail.com>
2024-02-26 17:22:13 -0800

    feat(contact): Refactor contact page to use CustomIcon component
    - Replaced GitHubIcon and LinkedInIcon with CustomIcon component
    - Created CustomIcon component for reusable icon rendering

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,14 +1,15 @@
-import GitHubIcon from "@/components/icons/GitHubIcon";
-import LinkedInIcon from "@/components/icons/LinkedInIcon";
-import Image from "next/image";
+import CustomIcon from "@/components/CustomIcon";
 
 const Contact = () => {
   return (
     <main className="flex flex-col p-5 z-0 overflow-y-auto w-full">
       <h1 className="text-5xl sm:text-6xl lg:text-8xl xl:text-9xl">Contact</h1>
       <div className="w-full flex  justify-around">
-        <GitHubIcon />
-        <LinkedInIcon />
+        <CustomIcon iconType="GitHub" link="https://github.com/zaqreynolds" />
+        <CustomIcon
+          iconType="LinkedIn"
+          link="https://www.linkedin.com/in/zaqreynolds/"
+        />
       </div>
     </main>
   );
---
+++ components/CustomIcon.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { useEffect, useState } from "react";
+import GitHubSvg from "./svgs/GitHubSvg";
+import LinkedInSvg from "./svgs/LinkedInSvg";
+
+const CustomIcon = ({
+  iconType,
+  link,
+  initialBgColor = "bg-default",
+  initialSvgColor = "#99b0a3",
+}: {
+  iconType: string;
+  link?: string;
+  initialBgColor?: string;
+  initialSvgColor?: string;
+}) => {
+  const [bgColor, setBgColor] = useState(initialBgColor);
+  const [svgColor, setSvgColor] = useState(initialSvgColor);
+
+  const handleMouseDown = () => setBgColor("bg-olive");
+  const handleMouseUp = () => setBgColor(initialBgColor);
+
+  useEffect(() => {
+    if (bgColor === "bg-olive") {
+      setSvgColor("#3D4C3F");
+    } else {
+      setSvgColor(initialSvgColor);
+    }
+  }, [bgColor, initialSvgColor]);
+
+  return (
+    <div
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
+      className={`flex ${bgColor} h-[37px] w-[37px] justify-center items-center rounded md:opacity-80 hover:opacity-100`}
+    >
+      <a href={link} target="_blank">
+        {iconType === "GitHub" && <GitHubSvg bgColor={svgColor} />}
+        {iconType === "LinkedIn" && <LinkedInSvg />}
+      </a>
+    </div>
+  );
+};
+
+export default CustomIcon;
