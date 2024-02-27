d3e1c493252e9e2065b59e0b08c62b20ee298001
Zaq Reynolds <zaqreynolds@gmail.com>
2024-02-27 15:39:58 -0800

    style(contact): Update contact page layout and add form for submissions
    Adjusted the layout of the contact page to include a submission form for users to reach out with project opportunities. Updated CustomIcon component styling for hover effect consistency.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,12 +4,36 @@ const Contact = () => {
   return (
     <main className="flex flex-col p-5 z-0 overflow-y-auto w-full">
       <h1 className="text-5xl sm:text-6xl lg:text-8xl xl:text-9xl">Contact</h1>
-      <div className="w-full flex  justify-around">
-        <CustomIcon iconType="GitHub" link="https://github.com/zaqreynolds" />
-        <CustomIcon
-          iconType="LinkedIn"
-          link="https://www.linkedin.com/in/zaqreynolds/"
-        />
+      <div className="flex-1 flex flex-col justify-between">
+        <div>
+          <p className="pb-2">
+            I&apos;m always open to new opportunities. Feel free to reach out to
+            and tell me a little bit about yourself or your project.
+          </p>
+          <form className="flex flex-col justify-center items-center">
+            <textarea
+              rows={10}
+              cols={30}
+              className="bg-green-100 text-darkOlive w-80 my-2"
+            />
+
+            <button
+              type="submit"
+              className="bg-olive text-default font-bold w-24 my-2 py-2 px-4 rounded"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+
+        <div className="flex px-2 justify-center">
+          <CustomIcon iconType="GitHub" link="https://github.com/zaqreynolds" />
+          <div className="w-10" />
+          <CustomIcon
+            iconType="LinkedIn"
+            link="https://www.linkedin.com/in/zaqreynolds/"
+          />
+        </div>
       </div>
     </main>
   );
