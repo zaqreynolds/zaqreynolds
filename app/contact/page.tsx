9f2001da333c7c4200d92befe2f03058301a9026
Zaq Reynolds <zaqreynolds@gmail.com>
2024-03-01 14:58:28 -0800

    style(contact): Update form layout and add form state management
    - Added form state using useState for name, email, message fields
    - Updated form layout with labels for each input field
    - Implemented button activation based on form completion

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,15 @@
+"use client";
 import CustomIcon from "@/app/components/CustomIcon";
+import { useState } from "react";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [isButtonActive, setButtonActive] = useState(false);
+
   return (
     <main className="flex flex-col p-5 z-0 overflow-y-auto w-full">
       <h1 className="text-5xl sm:text-6xl lg:text-8xl xl:text-9xl">Contact</h1>
@@ -11,15 +20,37 @@ const Contact = () => {
             and tell me a little bit about yourself or your project.
           </p>
           <form className="flex flex-col justify-center items-center">
-            <textarea
-              rows={10}
-              cols={30}
-              className="bg-green-100 text-darkOlive w-80 my-2"
-            />
-
+            <div className="flex flex-col justify-center items-start pb-2">
+              <label className="text-left">Name:</label>
+              <input
+                type="text"
+                placeholder="Name"
+                className="bg-green-100 text-darkOlive w-80 my-2"
+              />
+            </div>
+            <div className="flex flex-col justify-center items-start pb-2">
+              <label className="text-left">E-mail:</label>
+              <input
+                type="email"
+                placeholder="Email"
+                className="bg-green-100 text-darkOlive w-80 my-2"
+              />
+            </div>
+            <div className="flex flex-col justify-center items-start">
+              <label className="text-left">Message:</label>
+              <textarea
+                placeholder="Write your message here"
+                rows={10}
+                cols={30}
+                className="bg-green-100 text-darkOlive w-80 my-2"
+              />
+            </div>
             <button
               type="submit"
-              className="bg-olive text-default font-bold w-24 my-2 py-2 px-4 rounded"
+              disabled={!isButtonActive}
+              className={`bg-olive text-default font-bold w-24 my-2 py-2 px-4 rounded ${
+                isButtonActive ? "opacity-100" : "cursor-not-allowed opacity-50"
+              }`}
             >
               Submit
             </button>
