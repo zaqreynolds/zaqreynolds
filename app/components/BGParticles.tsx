806a70613c90a22cc1da88a879f54c69cc7e8f8b
Zaq Reynolds <zaqreynolds@gmail.com>
2024-08-22 15:29:56 -0700

    feat(components): Add BGParticles component
    - Added a new BGParticles component to render animated particles in the background using tsParticles library.
    - Initialized tsParticles engine with custom presets and options for particle animation.
    - Integrated BGParticles component into the layout for visual effect enhancement.

---
+++ app/components/BGParticles.tsx
@@ -0,0 +1,123 @@
+"use client";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { Engine } from "tsparticles-engine";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import {
+  type Container,
+  type ISourceOptions,
+  MoveDirection,
+  OutMode,
+} from "@tsparticles/engine";
+import { loadFirePreset } from "tsparticles-preset-fire";
+
+const BGPArticles: React.FC = () => {
+  const [init, setInit] = useState(false);
+
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFirePreset(engine);
+  }, []);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      // background: {
+      //   color: {
+      //     value: "#0d47a1",
+      //   },
+      // },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 100,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#3D4C3F",
+        },
+        // links: {
+        //   color: "3D4C3F",
+        //   distance: 150,
+        //   enable: true,
+        //   opacity: 3,
+        //   width: 1,
+        // },
+        move: {
+          direction: MoveDirection.none,
+          enable: true,
+          outModes: {
+            default: OutMode.out,
+          },
+          random: true,
+          speed: 3,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.4,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    );
+  }
+
+  return <></>;
+};
+
+export default BGPArticles;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Header from "@/app/components/Header";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
+import BGParticles from "./components/BGParticles";
 
 const spaceGrotesk = Space_Grotesk({
   weight: "400",
@@ -23,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${spaceGrotesk.className} text-lightOlive`}>
         <div className="h-screen w-screen flex flex-col overflow-hidden">
+          <BGParticles />
           <Header />
           <main className="flex flex-grow overflow-hidden">{children}</main>
         </div>
